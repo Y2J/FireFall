e8ca7e709bf36798a199f64a310d8987e8b7e2d7
y2jinc <y2jinc@gmail.com>
2015-07-27 18:30:20 +0900

    + FireFall Source / Resource 추가.

---
+++ Build/FireFall/Source/FireFall/FireFall.Build.cs
@@ -0,0 +1,11 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FireFall : ModuleRules
+{
+	public FireFall(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+	}
+}
