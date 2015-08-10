3bdbfcd17ba13a61261ec39c0daaabd2db817e92
y2jinc <y2jinc@gmail.com>
2015-08-10 18:24:32 +0900

    * FireFallProjectile 클래스 추가.

---
+++ Build/FireFall/Source/FireFall/FireFallProjectile.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "FireFallProjectile.generated.h"
+
+UCLASS()
+class FIREFALL_API AFireFallProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+	/** Sphere collision component */
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	class USphereComponent* CollisionComp;
+
+	/** Projectile movement component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class UProjectileMovementComponent* ProjectileMovement;
+
+	
+public:	
+	// Sets default values for this actor's properties
+	AFireFallProjectile();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	/** called when projectile hits something */
+	UFUNCTION()
+	void OnHit(AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+};
